5941397dc7ac5a9f5f063cd277c86350dc17565f
Alex <614647621@qq.com>
2022-08-16 16:43:52 +0800

    123

--- config/apiUrl.js
+++ config/apiUrl.js
@@ -5,9 +5,11 @@ let ipUrl = null; //接口
 if(process.env.APP_ENV?.trim()==="dev"){
     //开发以及测试环境接口地址(我这里开发环境和测试环境是一样的接口)
     ipUrl="http://127.0.0.1:7777/api/default"
+    console.log('开发环境=============>',)
 }else if(process.env.APP_ENV?.trim()==="pro") {
       //生产环境接口地址,http://127.0.0.1/api/default是服务器用nginx代理了http://127.0.0.1:7777端口
     ipUrl="http://127.0.0.1/api/default"  
+    console.log('生成环境=============>',)
 }
     
 
